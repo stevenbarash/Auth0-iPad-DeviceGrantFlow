0bddb10f4e6eb35c079b72f3f06f0e20759e5155
Steven Barash <steven.barash@okta.com>
2022-08-30 14:16:11 -0400

    Initial Commit

---
+++ port-deviceflow-ipadUITests/port_deviceflow_ipadUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  port_deviceflow_ipadUITestsLaunchTests.swift
+//  port-deviceflow-ipadUITests
+//
+//  Created by Steven Barash on 8/30/22.
+//
+
+import XCTest
+
+class port_deviceflow_ipadUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
